79a4953b0b329a45979b14d34b405c5044f4b6a3
BadiaRtimi <49767916+badiart@users.noreply.github.com>
2021-07-15 21:34:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express=require('express')
 const mongoose=require('mongoose')
 const dotenv = require('dotenv')
 const router=require('./routes/routes')
+
+
+
 dotenv.config()
+
+
 const app=express()
+
+
 app.use(express.json());
 //data base connection
 mongoose.connect(process.env.mongoString, {useNewUrlParser: true,useUnifiedTopology:true})
@@ -14,14 +21,11 @@ mongoose.connection.on("error", function(error) {
 mongoose.connection.on("open", function() {
   console.log("Connected to MongoDB database.")
 })
-//server 
-app.listen(process.env.Port,(error)=>{
-  if(error){console.log(error)}
-{console.log("server is running")}})
-
-// app.get('/',(req,res)=>{
-//   Person.find().then((el)=>res.json(el))
-// .catch((err)=>console.log(err)) 
-// })
 
 app.use('/api/users',require('./routes/routes'))
+//server creation 
+app.listen(5003,(error)=>{
+  if(error){console.log(error)}
+  else
+{console.log("server is running")}})
+
